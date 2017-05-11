b1e967daae442e83afdad60c8135fb6328d8be88
Alex <ardunn@lbl.gov>
2017-05-10 21:52:21 -0700

    second example, using Fabien's methods

---
+++ pyprostate/example_2.py
@@ -0,0 +1,66 @@
+import scipy.io
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+
+data = scipy.io.loadmat('data.mat')['data'][0]
+
+patient = data[0]
+mask = patient[0]
+t2 = patient[1]
+
+patient2 = data[-1]
+mask2 = patient2[0]
+t2_2 = patient2[1]
+
+def create_dataset(image, mask, n, prune=False):
+
+    subimages = []
+    y = []
+    for i in range(n, 255-n):
+        for j in range(n, 255-n):
+            if mask[i,j] != 0:
+
+                store = True
+                if prune:
+                    for v in mask[i-n:i+n+1, j-n:j+n+1]:
+                        if 0 in v:
+                            store = False
+                            break
+
+                if store:
+                    subimages.append(image[i-n:i+n+1, j-n:j+n+1])
+
+                    # translate (2 -> 1) and 1 -> 0
+                    yi = 1 if int(round(mask[i,j]))==2 else 0
+                    y.append(yi)
+
+    X = [image.flatten() for image in subimages]
+    return X, y
+
+
+print "making training data"
+X_train = []
+y_train = []
+
+for image in data[0:-1]:
+    mask_single = image[0]
+    t2_single = image[1]
+    X_train_single, y_train_single = create_dataset(t2_single, mask_single, 3, prune=True)
+    X_train += X_train_single
+    y_train += y_train_single
+
+print "making testing data"
+X_test, y_test = create_dataset(t2_2, mask2, 3, prune = True)
+
+print "training"
+classifier = RandomForestClassifier()
+classifier.fit(X_train, y_train)
+
+print "predicting"
+y_pred = classifier.predict(X_test)
+
+score = roc_auc_score(y_true=y_test, y_score=y_pred)
+print "score", score
+
+
